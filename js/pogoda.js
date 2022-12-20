4a95f31bca0310a344b9cd4bb5dc4086ffa9eb1d
Svetlana <svetlanalasiichyk@gmail.com>
2022-12-20 23:06:40 +0200

    Add

--- js/pogoda.js
+++ js/pogoda.js
@@ -4,37 +4,56 @@ function GetInfo() {
 	const cityName = document.getElementById("cityName");
 	cityName.innerHTML = "--" + newName.value + "--";
 
-	fetch('https://api.open-meteo.com/v1/forecast?latitude=' + newName.value + '=temperature_2m,windspeed_10m,winddirection_10m,windgusts_10m&start_date=2022-12-16&end_date=2022-12-19')
-		.then(response => response.json())
-		.then(data => renderWeather(data))
-		.catch(err => console.log(err));
-
-
-	function renderWeather(weather) {
-		const rootDiv = document.getElementById('card');
-
-		const temperatureEl = document.createElement('div');
-		const windspeedEl = document.createElement('div');
-		const winddirectionEl = document.createElement('div');
-		const windgustsEl = document.createElement('div');
-
-
-		rootDiv.append(temperatureEl, windspeedEl, winddirectionEl, windgustsEl);
-
-		temperatureEl.textContent = weather.hourly.temperature_2m;
-		windspeedEl.textContent = weather.hourly.windspeed_10m;
-		winddirectionEl.textContent = weather.hourly.winddirection_10m;
-		windgustsEl.textContent = weather.hourly.windgusts_10m;
-	}
 }
+//данные ввела в карточку но function GetInfo неработает
+//-город поставить не могу и температура не меняется
+// + newName.value +  надо поставить в ссылку -не получается или не там пытаюсь поставить
 
-//function renderWeather(weather) {
-//	for (i = 0; i < 3; i++) {
-//document.getElementById("day" + (i + 1) + "T").innerHTML = "T: " + Number(data.list[i].weather.hourly.temperature_2m).+ "°";
-//	}
-//}
+fetch(
+	"https://api.open-meteo.com/v1/forecast?latitude=47.85&longitude=35.12&daily=windspeed_10m_max,windgusts_10m_max&current_weather=true&timezone=Europe%2FMoscow&start_date=2022-12-20&end_date=2022-12-25"
+)
+	.then((response) => response.json())
+	.then((data) => renderWeather(data))
+	.catch((err) => console.log(err));
+
+function renderWeather(weather) {
+	console.log(weather.daily.windspeed_10m_max);
+	console.log(weather.daily.windgusts_10m_max);
 
 
+	//вывести  данные про ветер
+
+	const windSpeed1Day = document.querySelector(".windSpeed1");
+	const windSpeed1DayEl = document.createElement("p");
+	windSpeed1Day.append(windSpeed1DayEl);
+	windSpeed1DayEl.textContent = `${weather.daily.windspeed_10m_max[0]} км/год`;
+
+	const windSpeed2Day = document.querySelector(".windSpeed2");
+	const windSpeed2DayEl = document.createElement("p");
+	windSpeed2Day.append(windSpeed2DayEl);
+	windSpeed2DayEl.textContent = `${weather.daily.windspeed_10m_max[1]} км/год`;
+
+	const windSpeed3Day = document.querySelector(".windSpeed3");
+	const windSpeed3DayEl = document.createElement("p");
+	windSpeed3Day.append(windSpeed3DayEl);
+	windSpeed3DayEl.textContent = `${weather.daily.windspeed_10m_max[2]} км/год`;
+
+
+	const windGusts1Day = document.querySelector(".windGusts1");
+	const windGusts1DayEl = document.createElement("p");
+	windGusts1Day.append(windGusts1DayEl);
+	windGusts1DayEl.textContent = `${weather.daily.windgusts_10m_max[0]} км/год`;
+
+	const windGusts2Day = document.querySelector(".windGusts2");
+	const windGusts2DayEl = document.createElement("p");
+	windGusts2Day.append(windGusts2DayEl);
+	windGusts2DayEl.textContent = `${weather.daily.windgusts_10m_max[1]} км/год`;
+
+	const windGusts3Day = document.querySelector(".windGusts3");
+	const windGusts3DayEl = document.createElement("p");
+	windGusts3Day.append(windGusts3DayEl);
+	windGusts3DayEl.textContent = `${weather.daily.windgusts_10m_max[2]} км/год`;
+}
 
 function DefaultScreen() {
 	document.getElementById("cityInput").defaultValue = "kiev";
